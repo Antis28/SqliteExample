ff2624f19d3ff0719020d138dac4d4affa9d8f0d
Antis <Antis28@mail.ru>
2023-02-14 10:48:09 +0300

    add привязка данных

--- SqliteExample/Views/NoteAddingPage.xaml.cs
+++ SqliteExample/Views/NoteAddingPage.xaml.cs
@@ -1,15 +1,53 @@
 ﻿
+using SqliteExample.Models;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SqliteExample.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
     public partial class NoteAddingPage : ContentPage
     {
+        public string ItemId {set { LoadNote(value); } }
+
+        
+
         public NoteAddingPage()
         {
             InitializeComponent();
+            BindingContext = new Note();
+        }
+
+        private async void OnSaveButton_Clicked(object sender, System.EventArgs e)
+        {
+            var note = BindingContext as Note;
+            note.Date = DateTime.UtcNow;
+            if (!string.IsNullOrWhiteSpace(note.Text))
+            {
+                await App.NotesDB.SaveNoteAsync(note);
+            }
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnDeleteButton_Clicked(object sender, System.EventArgs e)
+        {
+            var note = BindingContext as Note;
+            await App.NotesDB.DeleteNoteAsync(note);
+            await Shell.Current.GoToAsync("..");
+        }
+        private async void LoadNote(string value)
+        {
+            try
+            {
+                var id = Convert.ToInt32(value);
+                var note = await App.NotesDB.GetNoteAsync(id);
+                BindingContext = note;
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
--- SqliteExample/Views/NotesPage.xaml.cs
+++ SqliteExample/Views/NotesPage.xaml.cs
@@ -1,15 +1,37 @@
 ﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using SqliteExample.Models;
+using System.Linq;
 
 namespace SqliteExample.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
     public partial class NotesPage : ContentPage
     {
         public NotesPage()
         {
             InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+            collectionView.ItemsSource = await App.NotesDB.GetNotesAsync();
+            base.OnAppearing();
+        }
+
+        private async void AddButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(NoteAddingPage));
+        }
+
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                var note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync(
+                    $"{nameof(NoteAddingPage)}?{nameof(NoteAddingPage.ItemId)}={note.Id.ToString()}");
+            }
         }
     }
 }
